39ea070c8bbac06eafe32ccd874f2d2b0af3ed84
skrodrigo <rodrigoa0987@gmail.com>
2024-05-11 19:12:24 -0300

    fix: default function footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,9 +1,11 @@
-export default function(){
-    return (
-        <>
-        <footer className="flex items-center justify-center mb-3 mt-auto">
-          <span className="font-normal text-sm">skrodrigo © 2024</span>
-        </footer> 
-        </>
-    )
+const footer = () => {
+  return (
+    <div>        
+      <footer className="flex items-center justify-center mb-3 mt-auto">
+        <span className="font-normal text-sm">skrodrigo © 2024</span>
+      </footer> 
+    </div>
+  )
 }
+
+export default footer
