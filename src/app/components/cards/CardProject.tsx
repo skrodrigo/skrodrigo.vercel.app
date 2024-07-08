9020fb830665e00fe8af86d34aee9d2b85fc46e8
skrodrigo <rodrigoa0987@gmail.com>
2024-07-08 12:02:34 -0300

    feat: create page techs

---
+++ src/app/components/cards/CardProject.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+interface CardProjectProps {
+  title: string;
+  description: string;
+  techs: string;
+  links: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const CardProject = ({ title, description, techs, links }: CardProjectProps) => {
+  return (
+    <div>
+      <div className="md:flex md:flex-row flex-col items-center justify-between border-2 mt-14 px-7 py-4 mx-auto border-[#232323] rounded-3xl w-[300px] md:w-[600px] lg:w-[900px] xl:w-[1216px] font-semibold">
+        <div className="flex flex-col items-start justify-start space-y-2">
+          <span>{title}</span>
+          <span className='text-[#2563EB]'>{description}</span>
+          <span className='text-[#6F6F6F] font-medium'>{techs}</span>
+        </div>
+
+        <div className="text-emerald-500 space-x-4 underline mt-3 md:mt-0">
+          {links.map((link, index) => (
+            <Link key={index} href={link.url} target="_blank">{link.name}</Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardProject;
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,11 +1,58 @@
-const page = () => {
+import CardProject from "../components/cards/CardProject";
+
+const projectsData = [
+  {
+    title: "Cariri Tips",
+    description: "Projeto Oficial para o site da CaririTips.",
+    techs: "#Next.Js #Shadcn/ui #Typescript #ResponsiveDesign #tailwindCSS ",
+    links: [
+      { name: "Deploy", url: "https://cariritips.vercel.app/" },
+      { name: "Figma", url: "https://www.figma.com/design/SWAMFkpQEGCKfqdJSBThVR/cariritips.com.br?node-id=0-1&t=7CphBOHg7IstZEb9-1" },
+      { name: "Código", url: "https://github.com/skrodrigo/cariritips.com.br" }
+    ]
+  },
+  {
+    title: "Portfólio Skrodrigo",
+    description: "Projeto do meu portfólio.",
+    techs: "#Next.Js #Typescript #TailwindCSS",
+    links: [
+      { name: "Deploy", url: "https://skrodrigo.vercel.app/" },
+      { name: "Figma", url: "https://www.figma.com/design/Hf7LgtuqtgcNPlZHEzJ7U3/portfolio?node-id=216-6&t=kCNO36uBvorM8fvR-1" },
+      { name: "Código", url: "https://github.com/skrodrigo/skrodrigo.vercel.app" }
+    ]
+  },
+  {
+    title: "Micro Framework",
+    description: "Projeto para criar um Micro Framework em SASS de um site Famoso. O escolhido foi: kirvano.",
+    techs: "#sass #figma #html",
+    links: [
+      { name: "", url: "" },
+      { name: "Figma", url: "https://www.figma.com/design/HFtXws11QdYxa22pMm6kSL/Style--Guide?node-id=0-1&t=jW1COt0BJVd92OZm-1" },
+      { name: "Código", url: "https://github.com/skrodrigo/design.system.kirvano" }
+    ]
+  }
+];
+
+const Page = () => {
   return (
+    <div className="mb-20">
 
-    <div className="flex items-center justify-center">
-      <span className="mt-16">em breve...</span>
+      <div className="flex flex-col items-center justify-center mt-12 space-y-8">
+        <span className="text-5xl font-semibold md:text-6xl">Projetos</span>
+        <p className="text-center mx-5">Visualize alguns dos meus <b>projetos</b>.</p>
+      </div>
+
+      {projectsData.map((project, index) => (
+        <CardProject
+          key={index}
+          title={project.title}
+          description={project.description}
+          techs={project.techs}
+          links={project.links}
+        />
+      ))}
     </div>
-          
-  )
-}
+  );
+};
 
-export default page
+export default Page;
