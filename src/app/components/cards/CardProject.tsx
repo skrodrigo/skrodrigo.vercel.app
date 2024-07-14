76395d7bb4732e0f6a7f8f4b9060d3e8d4c05f82
skrodrigo <rodrigoa0987@gmail.com>
2024-07-14 08:35:15 -0300

    fix: text card project large

--- src/app/components/cards/CardProject.tsx
+++ src/app/components/cards/CardProject.tsx
@@ -16,7 +16,7 @@ const CardProject = ({ title, description, techs, links }: CardProjectProps) => 
       <div className="md:flex md:flex-row flex-col items-center justify-between border-2 mt-14 px-7 py-4 mx-auto border-[#232323] rounded-3xl w-[300px] md:w-[600px] lg:w-[900px] xl:w-[1216px] font-semibold">
         <div className="flex flex-col items-start justify-start space-y-2">
           <span>{title}</span>
-          <span className='text-[#2563EB]'>{description}</span>
+          <span className='text-[#2563EB] max-w-lg'>{description}</span>
           <span className='text-[#6F6F6F] font-medium'>{techs}</span>
         </div>
 
